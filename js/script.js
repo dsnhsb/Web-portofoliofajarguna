8e5a6fd5d3963e631ada7e17a9d41b6939f1f5bb
dsnhsb <fajarguna150905@gmail.com>
2026-02-12 16:10:31 +0700

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-function showMessage() {
-    alert("Terima kasih telah mengunjungi portofolio saya!"); 
-}
-
-let judulElement = document.getElementById("judul"); 
-judulElement.style.color = "#ecf0f1"; 

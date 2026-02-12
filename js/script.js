fb920a1bf7178292b325be14c8115f312f0eb2b3
dsnhsb <fajarguna150905@gmail.com>
2026-02-12 16:04:00 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    alert("Terima kasih telah mengunjungi portofolio saya!"); 
+}
+
+let judulElement = document.getElementById("judul"); 
+judulElement.style.color = "#ecf0f1"; 
